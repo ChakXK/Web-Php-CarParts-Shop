2d9ef07ffc1c53e07b0eeedeb762fcda17740307
ChakXK <kvlchkksn@gmail.com>
2020-09-22 22:16:20 +0300

    Done

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://arturmij.beget.tech/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://arturmij.beget.tech/');
+
+// DIR
+define('DIR_APPLICATION', '/home/a/arturmij/autoshop/public_html/catalog/');
+define('DIR_SYSTEM', '/home/a/arturmij/autoshop/public_html/system/');
+define('DIR_IMAGE', '/home/a/arturmij/autoshop/public_html/image/');
+define('DIR_LANGUAGE', '/home/a/arturmij/autoshop/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/a/arturmij/autoshop/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/a/arturmij/autoshop/public_html/system/config/');
+define('DIR_CACHE', '/home/a/arturmij/autoshop/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/a/arturmij/autoshop/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/a/arturmij/autoshop/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/a/arturmij/autoshop/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/a/arturmij/autoshop/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'arturmij_au');
+define('DB_PASSWORD', 'JuaUSLd7');
+define('DB_DATABASE', 'arturmij_au');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'ash_');
